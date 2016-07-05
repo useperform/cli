ecf67fa66bb27b9c9283470f49cc90e5e9f9acc7
Glynn Forrest <me@glynnforrest.com>
2016-07-05 15:56:25 +0100

    Add support for creating files within bundles

--- src/FileCreator.php
+++ src/FileCreator.php
@@ -34,18 +34,21 @@ class FileCreator
 
     public function forceCreate($file)
     {
+        $vars = [];
+        $template = $file;
+
+        if (preg_match('`src/(.*Bundle)/`', $file, $matches)) {
+            $template = preg_replace('`src/.*Bundle/`', 'src/_bundle/', $file);
+            $vars['bundle_namespace'] = str_replace('/', '\\', $matches[1]);
+        }
+
         if (!file_exists(dirname($file))) {
             mkdir(dirname($file), 0755, true);
         }
 
-        file_put_contents($file, $this->render($file));
+        file_put_contents($file, $this->twig->render($template.'.twig', $vars));
         if (isset($this->chmods[$file])) {
             chmod($file, $this->chmods[$file]);
         }
-    }
-
-    public function render($file)
-    {
-        return $this->twig->render($file.'.twig');
     }
 }
