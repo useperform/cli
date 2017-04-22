97528e306ff13807bbc22fff28b9dfc57bbfd8ad
Glynn Forrest <me@glynnforrest.com>
2017-04-22 22:30:09 +0100

    cmd: remove all file creation commands
    All file creation lives in the PerformDevBundle now

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -3,8 +3,6 @@
 namespace Perform\Cli\Command;
 
 use Symfony\Component\Console\Command\Command as BaseCommand;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\ArrayInput;
 
 /**
  * Command.
@@ -21,15 +19,5 @@ class Command extends BaseCommand
     protected function get($name)
     {
         return $this->getContainer()[$name];
-    }
-
-    protected function createFile(OutputInterface $output, $file, $skipExisting = false)
-    {
-        $command = $this->getApplication()->find('create:file');
-        $args = new ArrayInput([
-            'file' => $file,
-            '--skip-existing' => $skipExisting,
-        ]);
-        $command->run($args, $output);
     }
 }
--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -20,66 +20,12 @@ class InitCommand extends Command
     protected function configure()
     {
         $this->setName('init')
-            ->setDescription('Create a new perform application in the current directory')
-            ->addOption(
-                'skip-existing',
-                's',
-                InputOption::VALUE_NONE,
-                'Don\'t prompt to overwrite files that already exist.'
-            )
+            ->setDescription('Create and configure a new perform application in the current directory.')
             ;
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<info>Creating application files</info>');
-        $files = [
-            '.bowerrc',
-            '.gitignore',
-            'Makefile',
-            'bower.json',
-            'composer.json',
-            'gulpfile.js',
-            'package.json',
-
-            'app/autoload.php',
-            'app/AppCache.php',
-            'app/AppKernel.php',
-            'app/config/config.yml',
-            'app/config/config_dev.yml',
-            'app/config/config_prod.yml',
-            'app/config/parameters.yml.dist',
-            'app/config/routing.yml',
-            'app/config/routing_dev.yml',
-            'app/config/security.yml',
-            'app/Resources/PerformCmsBundle/views/scripts.html.twig',
-            'bin/console',
-            'var/cache/.gitkeep',
-            'var/logs/.gitkeep',
-            'var/sessions/.gitkeep',
-            'web/.htaccess',
-            'web/app.php',
-            'web/app_dev.php',
-            'web/assets/scss/app.scss',
-            'web/assets/scss/variables.scss',
-            'web/assets/scss/vendors.scss',
-        ];
-        foreach ($files as $file) {
-            $this->createFile($output, $file, $input->getOption('skip-existing'));
-        }
-
-        $this->maybeCreateBundle($input, $output);
-    }
-
-    protected function maybeCreateBundle(InputInterface $input, OutputInterface $output)
-    {
-        $question = new ConfirmationQuestion("Create a new bundle for this app? ", false);
-        $create = $this->getHelper('question')->ask($input, $output, $question);
-        if (!$create) {
-            return;
-        }
-
-        $command = $this->getApplication()->find('create:bundle');
-        $command->run(new ArrayInput([]), $output);
+        //composer create-project
     }
 }
